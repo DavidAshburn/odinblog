26881bcce5f28523b7dcb2cbc2c2e9adc877e11a
DavidAshburn <oneinchwebbing@gmail.com>
2022-09-25 18:58:40 -0700

    controllers and nav/alert partials

---
+++ app/javascript/controllers/navbar_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+static targets = [ "maindrop", "altdrop" ];
+
+  initialize() {
+  }
+
+  connect() {
+  }
+
+  showDrop() {
+    this.maindropTarget.classList.toggle("hidden", false);
+  }
+
+  showDropAlt() {
+    this.altdropTarget.classList.toggle("hidden", false);
+  }
+
+  hideDrop() {
+    this.maindropTarget.classList.toggle("hidden", true);
+  }
+
+  hideDropAlt() {
+    this.altdropTarget.classList.toggle("hidden", true);
+  }
+
+}
